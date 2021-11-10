8bc7b4c160aa41abcd2e872cf6bf6c196d8902ab
petya <mityushin_petya@mail.ru>
2021-11-10 22:37:20 +0300

    start development AudiendDatabase

--- app/src/main/java/com/example/equimpcontrol/database/AudiencDatabase.kt
+++ app/src/main/java/com/example/equimpcontrol/database/AudiencDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.equimpcontrol.database
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
@@ -9,10 +11,57 @@ class AudiencDatabase(var context : Context) : SQLiteOpenHelper(context, "", nul
     private val DB_NAME = "audiences.db"
     private var DB_TABEL = ""
 
-    public fun checkAudiences(audiencNumber : Int)
+    @SuppressLint("Range")
+    public fun checkAudiences(audiencNumber : Int, db : SQLiteDatabase?)
     {
+        DB_TABEL = "Список аудиторий"
+        val query : String = "SELECT * FROM " + DB_TABEL
+        val cursor : Cursor = db!!.rawQuery(query, null)
+        while (cursor.moveToNext()) {
+            if (audiencNumber == cursor.getInt(cursor.getColumnIndex("Номер аудитории"))) {
 
+            }
+        }
     }
+
+    @SuppressLint("Range")
+    private fun srcSoftwareAudiences(idAudienceAssign : Int, db : SQLiteDatabase?) : String // назначение аудитории
+    {
+        DB_TABEL = "Аудитория"
+        val query : String = "SELECT * FROM " + DB_TABEL
+        val cursor : Cursor = db!!.rawQuery(query, null)
+        while (cursor.moveToNext()) {
+            if (idAudienceAssign == cursor.getInt(cursor.getColumnIndex("ID номер аудитории"))) {
+                return cursor.getString(cursor.getColumnIndex("ПО"))
+            }
+        }
+        return ""
+    }
+
+    @SuppressLint("Range")
+    private fun srcHardwareAudience(idCharacter : Int, db : SQLiteDatabase?) : String // характеристика -> переферия
+    {
+        DB_TABEL = "Характеристика"
+        var id_hardware : Int? = null
+        var query : String = "SELECT * FROM " + DB_TABEL
+        var cursor : Cursor = db!!.rawQuery(query, null)
+        while (cursor.moveToNext()) {
+            if (idCharacter == cursor.getInt(cursor.getColumnIndex("ID характеристики"))) {
+                id_hardware = cursor.getInt(cursor.getColumnIndex("ID переферии"))
+            }
+        }
+        DB_TABEL = "Переферия"
+        query = "SELECT * FROM " + DB_TABEL
+        cursor = db!!.rawQuery(query, null)
+        while (cursor.moveToNext()) {
+            if (id_hardware == cursor.getInt(cursor.getColumnIndex("ID переферии"))) {
+                
+            }
+        }
+        return ""
+    }
+
+    //сделай функцию makeCursore
 
     override fun onCreate(db: SQLiteDatabase?) {
         TODO("Not yet implemented")
