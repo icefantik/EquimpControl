d10c79a77bb6a59ddcf8879557c994fdcb149714
petya <mityushin_petya@mail.ru>
2021-11-10 21:22:56 +0300

    fixed transitions for activity

--- app/src/main/java/com/example/equimpcontrol/EditActivity.kt
+++ app/src/main/java/com/example/equimpcontrol/EditActivity.kt
@@ -1,12 +1,19 @@
 package com.example.equimpcontrol
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class EditActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+        val saveButton : Button = findViewById(R.id.saveButton)
+        saveButton.setOnClickListener {
+            val intent = Intent(this@EditActivity, SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
